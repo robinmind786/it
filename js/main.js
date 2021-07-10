e8e45388bf158ba70a19445b3198a36c258e6cc4
Robin Mind <43693130+robinmind786@users.noreply.github.com>
2021-07-10 14:17:43 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,7 +9,38 @@ function openCity(evt, cityName) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   document.getElementById(cityName).style.display = "flex";
+  // document.getElementById(cityName).style.flexWrap = "wrap";
   evt.currentTarget.className += " active";
 }
 
 // document.getElementById("defaultOpen").click();
+
+
+
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+const menuWrapper = document.querySelector(".menu-wrapper");
+const hasCollapsible = document.querySelectorAll(".has-collapsible");
+
+// Sidenav Toggle
+openMenu.addEventListener("click", function () {
+  menuWrapper.classList.add("offcanvas");
+});
+
+closeMenu.addEventListener("click", function () {
+  menuWrapper.classList.remove("offcanvas");
+});
+
+// Collapsible Menu
+hasCollapsible.forEach(function (collapsible) {
+  collapsible.addEventListener("click", function () {
+    collapsible.classList.toggle("active");
+
+    // Close Other Collapsible
+    hasCollapsible.forEach(function (otherCollapsible) {
+      if (otherCollapsible !== collapsible) {
+        otherCollapsible.classList.remove("active");
+      }
+    });
+  });
+});
